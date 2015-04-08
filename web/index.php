8c90fb45daf0a825eca5f643aa38c967450857c5
GN Trifanoff <aznaum@gmail.com>
2015-04-08 15:30:07 -0700

    molding, shaping, forming...

--- web/index.php
+++ web/index.php
@@ -2,10 +2,10 @@
 
 require( 'horatio.php' );
 
-Logger::quicklog( "inside the belly" );
-
-$p = new Moldscape( 'holamundo' );
+$p = new Moldscape( 'default' );
 $p->set( 'title', 'zippyzo' );
+$p->set( 'pagetitle', 'welcome' );
+$p->set( 'pagebody', @$_GET['PATH_INFO'] );
 echo $p->generate();
 
 ?>
---
+++ web/views/default.tpl
@@ -0,0 +1,7 @@
+<article>
+<h2>{{pagetitle}}</h2>
+<div id="pagebody">
+{{pagebody}}
+</div>
+</article>
+
