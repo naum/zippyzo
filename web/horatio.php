407f6aac5abf944893811667035ec0c88def39ef
GN Trifanoff <aznaum@gmail.com>
2015-04-11 13:30:51 -0700

    minor formatting

--- web/horatio.php
+++ web/horatio.php
@@ -5,7 +5,7 @@ require( 'config.php' );
 function __autoload( $classname ) {
     $modulename = $_SERVER['DOCUMENT_ROOT'] . '/genus/' . strtolower( $classname ) . '.php';
     if (file_exists( $modulename )) {
-      require( $modulename );
+        require( $modulename );
     }
 }
 
