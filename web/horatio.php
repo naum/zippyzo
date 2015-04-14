8e07f84af01605199d38c85b50ea91ca57aea4bb
George Trifanoff <gtrifanoff@wbooth.local>
2015-04-13 23:02:00 -0700

    more minor formatting

--- web/horatio.php
+++ web/horatio.php
@@ -3,7 +3,9 @@
 require( 'config.php' );
 
 function __autoload( $classname ) {
-    $modulename = $_SERVER['DOCUMENT_ROOT'] . '/genus/' . strtolower( $classname ) . '.php';
+    $modulename = $_SERVER['DOCUMENT_ROOT'] 
+                  . '/genus/' . strtolower( $classname ) 
+                  . '.php';
     if (file_exists( $modulename )) {
         require( $modulename );
     }
